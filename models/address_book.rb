8dd860de9173a6878826b024e1158cead7307f6e
Merin Joseph <merinsony18@gmail.com>
2017-08-12 12:26:39 -0700

    assignment 7 search completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -60,5 +60,14 @@ class AddressBook
      # #5
      return nil
     end
+
+    def iterative_search(name)
+      entries.each do |entry|
+        if entry.name == name
+          return entry.name
+        end
+      end
+       return nil
+    end       
 end
     
